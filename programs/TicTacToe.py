3b0bae073adad2fc58c9ea27165e928b823baec4
Gaurav <shahgauravraj@gmail.com>
2021-03-01 11:50:41 +0530

    Tic Tac Toe modified

--- programs/TicTacToe.py
+++ programs/TicTacToe.py
@@ -1,69 +1,84 @@
-def evaluate(b) :
-    for i in range(3) :
-        if b[i][0]==b[i][1] and b[i][1]==b[i][2] :
-            if b[i][0]=='x' :
-                return 1
-            elif b[i][0]=='o' :
-                return -1
-        if b[0][i]==b[1][i] and b[1][i]==b[2][i] :
-            if b[0][i]=='x' :
-                return 1
-            elif b[0][i]=='o' :
-                return -1
-    if b[0][0]==b[1][1] and b[1][1]==b[2][2] :
-        if b[0][0]=='x' :
-            return 1
-        elif b[0][0]=='o' :
-            return -1
-    if b[0][2]==b[1][1] and b[1][1]==b[2][0] :
-        if b[0][2]=='x' :
-            return 1
-        elif b[0][2]=='o' :
-            return -1
-    return 0
+#Tic Tac Toe game using python
 
-def ismovesleft(b) :
-    for i in range(3) :
-        for j in range(3) :
-            if b[i][j]!='x' and b[i][j]!='o' :
-                return True
-    return False
+sq = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-def displayboard(b,turn) :
-    print("\tTic-Tac-Toe")
-    print("Player 1 (x) , Player 2 (o)")
-    print("Turn : Player ",turn)
-    for i in range(3) :
-        for j in range(3) :
-            print(" ",b[i][j],end="")
-        print()
+def board():
+    print('\n\tTic Tac Toe')
+    print('Player 1 (X) - Player 2 (0)')
+    print('   |   |   ')
+    print('',sq[1],'|',sq[2],'|',sq[3])
+    print('___|___|___')
+    print('   |   |   ')
+    print('',sq[4],'|',sq[5],'|',sq[6])
+    print('___|___|___')
+    print('   |   |   ')
+    print('',sq[7],'|',sq[8],'|',sq[9])
+    print('   |   |   ')
+
+def game_status():
+    if sq[1] == sq[2] and sq[2] == sq[3]:
+        return 1
+    elif sq[4] == sq[5] and sq[5] == sq[6]:
+        return 1
+    elif sq[7] == sq[8] and sq[8] == sq[9]:
+        return 1
+    elif sq[1] == sq[4] and sq[4] == sq[7]:
+        return 1
+    elif sq[2] == sq[5] and sq[5] == sq[8]:
+        return 1
+    elif sq[3] == sq[6] and sq[6] == sq[9]:
+        return 1
+    elif sq[1] == sq[5] and sq[5] == sq[9]:
+        return 1
+    elif sq[3] == sq[5] and sq[5] == sq[7]:
+        return 1
+    elif sq[1] != 1 and sq[2] != 2 and sq[3] != 3 and sq[4] != 4 and sq[5] != 5 and sq[6] != 6 and sq[7] != 7 and sq[8] != 8 and sq[9] != 9:
+        return 0
+    else:
+        return -1
     
-def checkboard(b,c) :
-    for i in range(3) :
-        for j in range(3) :
-            if c==b[i][j] :
-                return c
-    c=int(input("Invalid Choice , Enter Again (1-9) : "))
-    return checkboard(b,c)
-
-nextgame=True
-while nextgame :
-    board=[[1,2,3],[4,5,6],[7,8,9]]
-    turn=1
-    while True :
-        displayboard(board,turn)
-        choice=int(input("Enter number (1-9) : "))
-        choice=checkboard(board,choice)
-        board[(choice-1)//3][(choice-1)%3]='x' if turn==1 else 'o'
-        result=evaluate(board)
-        turn=2 if turn==1 else 1
-        if result==1 :
-            print("------ Player 1 wins ------")
-            break
-        elif result==-1 :
-            print("------ Player 2 wins ------")
-            break
-        if not ismovesleft(board) :
-            print("------ -----DRAW----- ------")
-            break
-    nextgame=int(input("Press 1 and enter for next game : "))
+player = 1
+status = -1
+    
+while status == -1:
+    board()
+    if player % 2 == 1:
+        player = 1
+    else:
+        player = 2
+    print('\nPlayer', player)
+    choice = int(input('Enter a number: '))
+    if player == 1:
+        mark = 'X'
+    else:
+        mark = '0'
+    
+    if choice == 1 and sq[1] == 1:
+        sq[1] = mark
+    elif choice == 2 and sq[2] == 2:
+        sq[2] = mark
+    elif choice == 3 and sq[3] == 3:
+        sq[3] = mark
+    elif choice == 4 and sq[4] == 4:
+        sq[4] = mark
+    elif choice == 5 and sq[5] == 5:
+        sq[5] = mark
+    elif choice == 6 and sq[6] == 6:
+        sq[6] = mark
+    elif choice == 7 and sq[7] == 7:
+        sq[7] = mark
+    elif choice == 8 and sq[8] == 8:
+        sq[8] = mark
+    elif choice == 9 and sq[9] == 9:
+        sq[9] = mark
+    else:
+        print('Invalid move')
+        player -= 1
+    status = game_status()
+    player += 1
+    
+print("RESULT")
+if status == 1:
+    print('Player', player-1, 'win')
+else:
+    print('Game draw')
